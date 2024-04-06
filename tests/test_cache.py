0c938b9ae4c17c2a0579d9906c795d0c09852a03
AlexandreSenpai <alexandrebsramos@hotmail.com>
2024-04-05 22:24:59 -0300

    tests: add sys.path to cache test file

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
 import time
 from enma.infra.core.utils.cache import Cache
 
