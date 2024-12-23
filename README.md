5ad3c8f9669057326497a05d0da1612ae2c99aba
AlexandreSenpai <alexandrebsramos@hotmail.com>
2024-12-23 18:32:47 -0300

    docs: update NHentaiConfig example

--- README.md
+++ README.md
@@ -70,9 +70,14 @@ from enma import Enma, Sources, CloudFlareConfig, NHentai
 
 enma = Enma[Sources]() # or just Enma()
 
-config = CloudFlareConfig(
-    user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-    cf_clearance=''
+config = NHentaiConfig(
+    cloudflare=CloudFlareConfig(
+        user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        cf_clearance=''
+    ),
+    source=NhentaiSourceConfig(
+        tiny_image_server='t1'
+    ) # You can specify source configs if available
 )
 
 enma.source_manager.set_source(source_name=Sources.NHENTAI)
--- enma/__init__.py
+++ enma/__init__.py
@@ -2,7 +2,7 @@ import sys
 from enma.application.core.utils.logger import LogMode, logger
 from enma.application.use_cases.download_chapter import Threaded
 from enma.infra.entrypoints.lib import Enma, Sources
-from enma.infra.adapters.repositories.nhentai import CloudFlareConfig, NHentai, Sort as NHentaiSort
+from enma.infra.adapters.repositories.nhentai import CloudFlareConfig, NHentai, Sort as NHentaiSort, NhentaiSourceConfig, NHentaiConfig
 from enma.infra.adapters.repositories.mangadex import Mangadex, Sort as MangadexSort
 from enma.infra.adapters.repositories.manganato import Manganato
 from enma.infra.adapters.downloaders.default import DefaultDownloader
@@ -16,6 +16,7 @@ from enma.domain.entities.search_result import SearchResult
 from enma.domain.entities.pagination import Pagination
 from enma.domain.entities.author_page import AuthorPage
 from enma.application.core.interfaces.manga_repository import IMangaRepository
+from enma._version import __version__
 
 package_name = "enma"
 python_major = "3"
